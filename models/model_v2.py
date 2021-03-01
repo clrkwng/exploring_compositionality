4b400c2f027d76584d17821b2719d61920e83511
Clark <clwang@berkeley.edu>
2021-03-01 13:08:28 -0800

    some refactoring

--- models/model_v2.py
+++ models/model_v2.py
@@ -22,24 +22,27 @@ class LBDLayer(nn.Module):
 		self.dropout = nn.Dropout(drop_p)
 
 	def forward(self, x):
-		x = self.relu(self.linear(x))
+		x = self.linear(x)
+		x = self.relu(x)
 
 		if self.batch_flag:
 			x = self.batchnorm(x)
 
 		x = self.dropout(x)
+
 		return x
 
 class WangNet(nn.Module):
-	def __init__(self, emb_dims, no_of_cont, lin_layer_sizes, output_size, hidden_drop_p, batch_flag):
+	def __init__(self, boolvec_dim, emb_dims, num_cont, lin_layer_sizes, output_size, hidden_drop_p, batch_flag):
 		super().__init__()
 		
 		self.embed_dim = emb_dims[1]
-		self.embedder = nn.Embedding(num_embeddings=emb_dims[0], embedding_dim=self.embed_dim)
-		# self.normalize_input = nn.BatchNorm1d(no_of_cont, affine=False)
+		if self.embed_dim != 0:
+			self.embedder = nn.Embedding(num_embeddings=emb_dims[0], embedding_dim=self.embed_dim)
+		# self.normalize_input = nn.BatchNorm1d(num_cont, affine=False)
 
 		# Can implement varying dropout_p for each layer (maybe input and hidden layers will differ) later on.
-		input_size = self.embed_dim + no_of_cont
+		input_size = self.embed_dim * boolvec_dim + num_cont
 		self.lbd_layers = nn.ModuleList([LBDLayer(input_size, lin_layer_sizes[0], batch_flag, hidden_drop_p)] + \
 																	[LBDLayer(lin_layer_sizes[i], lin_layer_sizes[i+1], batch_flag, hidden_drop_p) for i in range(len(lin_layer_sizes) - 1)])
 
@@ -48,11 +51,11 @@ class WangNet(nn.Module):
 
 	def forward(self, cont_data, cat_data):
 		# cont_data = self.normalize_input(cont_data)
-
 		if self.embed_dim != 0:
 			# x = [self.emb[xi] for xi in cat_data]
 			# x = torch.stack(x).squeeze()
 			x = self.embedder(cat_data.long())
+			x = x.reshape(cat_data.shape[0], -1)
 			x = torch.cat([cont_data, x], dim=1)
 		else:
 			x = cont_data
